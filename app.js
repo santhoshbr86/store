81499e309dcfb084f7f1e7cca91b6724f1134347
santhoshbr86 <santhoshbr86@gmail.com>
2024-02-25 06:16:14 -0500

    dad

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(bodyParser.json());
 connectDB();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
